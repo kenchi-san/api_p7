c5bf478830cf3314649fe4e15b773a02497c54f2
hugo charon <charon.hugo@gmail.com>
2021-11-10 16:49:55 +0100

    cleaning code

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,13 +5,11 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserController extends AbstractController
 {
